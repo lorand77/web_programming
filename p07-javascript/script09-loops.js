8937f01d423f2f6434a873272961c3a852582a30
lorand <lorand77@users.noreply.github.com>
2026-04-02 07:14:54 +0000

    loops

---
+++ p07-javascript/script09-loops.js
@@ -0,0 +1,11 @@
+for (let i = 10; i >= 0; i--) {
+    console.log(i);
+}
+
+console.log("----------------------------------------------------");
+
+let j = 10;
+while (j >= 0) {
+    console.log(j);
+    j--;
+}
