34d1a6972afaef661debc2632dce441ff144f234
lorand <lorand77@users.noreply.github.com>
2026-04-02 08:16:11 +0000

    revision

---
+++ p07-javascript/script10-revision.js
@@ -0,0 +1,7 @@
+numbers = [1, 2, 37, 45, 75, 100, 295, 1776, 1813, 580];
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0 || numbers[i] % 37 === 0) {
+        console.log(numbers[i]);
+    }
+}
