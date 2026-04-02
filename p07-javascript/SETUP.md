7043c7e85276fa74addb3c012b713646578d129c
lorand <lorand77@users.noreply.github.com>
2026-04-02 08:34:08 +0000

    ESLint

---
+++ p07-javascript/SETUP.md
@@ -0,0 +1,7 @@
+
+## install ESLint VSC extension
+
+## in terminal in the project folder, run:
+
+npm install --save-dev eslint
+npx eslint --init 
--- p07-javascript/script10-revision.js
+++ p07-javascript/script10-revision.js
@@ -1,4 +1,4 @@
-numbers = [1, 2, 37, 45, 75, 100, 295, 1776, 1813, 580];
+let numbers = [1, 2, 37, 45, 75, 100, 295, 1776, 1813, 580];
 
 for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] % 2 === 0 || numbers[i] % 37 === 0) {
