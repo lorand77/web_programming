9e673e5e70cb1744295afbffcedb87157e22517c
lorand <lorand77@users.noreply.github.com>
2026-04-02 07:48:03 +0000

    loops

--- p07-javascript/script09-loops.js
+++ p07-javascript/script09-loops.js
@@ -9,3 +9,28 @@ while (j >= 0) {
     console.log(j);
     j--;
 }
+
+const cars = ["Toyota", "Honda", "Ford"];
+
+for (let i = 0; i < cars.length; i++) {
+    console.log(cars[i]);
+}
+
+
+for (let i = 0; i <= 10; i += 2) {
+    console.log(i);
+}
+
+for (let i = 0; i <= 10; i += 2) {
+    if (i > 5) {
+        break;
+    }
+    console.log(i);
+}
+
+for (let i = 0; i <= 10; i += 2) {
+    if (i === 6) {
+        continue;
+    }
+    console.log(i);
+}
