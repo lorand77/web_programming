8f89bc7a3d1e6c251c86324f1d6c58762c1a7002
lorand <lorand77@users.noreply.github.com>
2026-04-19 08:05:31 +0000

    DOM

---
+++ p07-javascript/script22-debugging.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// let x;
+// console.log(x.length);
+
+
+// function f() {
+//     let firstName, lastName, discount, fullPrice;
+
+//     // Use later
+//     firstName = "John";
+//     lastName = "Doe";
+
+//     price = 19.90;
+//     discount = 0.10;
+
+//     fullPrice = price - discount;
+
+//     console.log(firstName);
+//     console.log(lastName);
+//     console.log(price);
+//     console.log(discount);
+//     console.log(fullPrice);
+// }
+
+
+// f();
+
+// console.log(price);
+
+
+const x = [1, 2, 3];
+console.log(x);
+
+x[0]=11;
+console.log(x);
+
+const y = 1;
+console.log(y);
+
+// y = 11; // TypeError: Assignment to constant variable.
+
+
+let xxx = 0;
+if (xxx = 10) {
+    console.log("x is 10");
+}
+
+console.log(xxx);
