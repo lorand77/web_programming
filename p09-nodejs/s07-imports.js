6f80f2d42f4ce5a5d0538ab01dbc8bd1e275e87f
lorand <lorand77@users.noreply.github.com>
2026-04-30 17:19:12 +0000

    Add utils module for mathematical constants and functions

---
+++ p09-nodejs/s07-imports.js
@@ -0,0 +1,16 @@
+// const { pi, areaOfCircle } = require('./utils');
+
+// console.log(pi);
+// console.log(areaOfCircle(2));
+
+
+
+const utils = require('./utils');
+
+console.log(utils.pi);
+console.log(utils.areaOfCircle(2));
+
+
+const uc = require('upper-case');
+
+console.log(uc.upperCase('hello world')); 
---
+++ p09-nodejs/utils.js
@@ -0,0 +1,9 @@
+const pi = 3.14159265358979;
+
+function areaOfCircle(r) {
+    return pi * r * r;
+}
+
+module.exports = {
+    pi, areaOfCircle
+};
