6e84697b28c2ca6881cb8ca46b39f37fc2e48690
lorand <lorand77@users.noreply.github.com>
2026-05-06 17:05:53 +0000

    http module

--- p12-core_modules/s01-http.js
+++ p12-core_modules/s01-http.js
@@ -1,16 +1,25 @@
-let http = require('http');
+const http = require('http');
+const url = require('url');
 
 http.createServer(function (req, res) {
+    // console.log(`Request Headers: ${req.headers}`);   // [object Object]
     console.log('Request Headers:', req.headers);
+    console.log(`Request Headers: ${JSON.stringify(req.headers)}`);
 
-    const { url, method } = req;
-    console.log(`Received ${method} request for ${url}`);
-    
+    console.log(`Host: ${req.headers.host}`);
+    console.log(`Method: ${req.method}`);
+    console.log(`URL: ${req.url}`);
+
+    const parsedUrl = url.parse(req.url, true);
+    console.log(`Pathname: ${parsedUrl.pathname}`);
+    console.log(`Query: ${JSON.stringify(parsedUrl.query)}`);
+    // console.log('Query: ', parsedUrl.query);   // [Object: null prototype] { v: '1' }
+
     res.writeHead(200, {
         'Content-Type': 'text/html',
         'Set-Cookie': 'sessionid=abc123; HttpOnly'
     });
-    res.end('<h1>Hello World!</h1>');
+    res.end('<h1>Hello World!</h1>\n');
     
 }).listen(8080);
 
